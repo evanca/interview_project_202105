b332e31ce7da79f8ebde7d9b094d0a366af60ef5
Ivanna <ecsaine@gmail.com>
2021-05-20 16:47:51 +0300

    Add a factory constructor that creates a user from JSON

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,12 +4,20 @@ class User {
   String phone;
   String address;
 
-  User(this.name, this.email, this.phone, this.address);
+  User({this.name, this.email, this.phone, this.address});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        name: json['name'],
+        email: json['email'],
+        phone: json['phone'],
+        address: "${json['address']['city']}, ${json['address']['street']}");
+  }
 
   String getInitials(String name) =>
       name.trim().split(' ').map((l) => l[0]).take(2).join();
 
-  Map<String, String> get toMap => {
+  Map<String, dynamic> get toMap => {
         "name": name,
         "initials": getInitials(name),
         "email": email,
